e52688eda365bf0734fa81267e790f1596ff2d59
Javier Cisneros L <lucatero@hotmail.com>
2022-03-01 11:03:17 -0600

    descripcion modificada

--- carreras.py
+++ carreras.py
@@ -4,7 +4,7 @@ from odoo import models,fields
 class carreras(models.Model):
     _name = 'escolares.carreras'
 
-    name = fields.Char(string='Clave de la carrera', size=10, required="True")
+    name = fields.Char(string='Clave de la carrera X', size=10, required="True")
     nombre = fields.Char(string='Clave de la carrera', size=60, required="True")
 
     _sql_constraints = [
