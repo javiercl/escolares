b222837601ccc61040cea4dd1d314d61883a3279
Javier Cisneros L <lucatero@hotmail.com>
2022-03-01 10:58:27 -0600

    first commit

---
+++ alumnos.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from odoo import models,fields
+
+class alumnos(models.Model):
+    _name = 'escolares.alumnos'
+
+    name = fields.Char(string='NControl')
+    nombre = fields.Char(string='Nombre')
+    direccion = fields.Char(string='Dirección')
+    telefono = fields.Char(string='Teléfono')
+    email = fields.Char(string='Email')
+    sexo = fields.Selection([('f','Femenino'),('m','Masculino')],string='Sexo')
+    edad = fields.Integer(string='Edad')
+    inscrito = fields.Boolean(string='Inscrito?')
+    carrera_id = fields.Many2one('escolares.carreras',string="Carrera")
+
+    _sql_constraints = [
+        ('unique_alumno', 'unique (name)', 'El alumno ya existe!')
+    ]
+
