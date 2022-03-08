eefaa531a516f7b9d4c0c27d61b1545e9428bd0a
GascaxD <al18020089@itsa.edu.mx>
2022-03-08 11:50:47 -0600

    comitgasca

--- __manifest__.py
+++ __manifest__.py
@@ -4,7 +4,7 @@
     'version': '1.0',
     'category': 'Universidades',
     'description': 'Control Escolar',
-    'author': 'Javier Cisneros,Jorge Cerda,Enrique Huerta,Cesar Soria,Geronimo',
+    'author': 'Javier Cisneros,Jorge Cerda,Enrique Huerta,Cesar Soria,Geronimo,Gasca Jesus',
     'maintainer': 'ITSA',
     'website': 'http://www.itsa.edu.mx',
     'depends': ['base'],
