c02f657056c44efd7a0177e5d9f6f8fb2347986d
IMAC5 <pc5@iMac-de-IMAC5.local>
2022-03-08 11:32:47 -0600

    modificacion geronimo

--- __manifest__.py
+++ __manifest__.py
@@ -4,7 +4,7 @@
     'version': '1.0',
     'category': 'Universidades',
     'description': 'Control Escolar',
-    'author': 'Javier Cisneros,Jorge Cerda,Enrique Huerta,Cesar Soria',
+    'author': 'Javier Cisneros,Jorge Cerda,Enrique Huerta,Cesar Soria,Geronimo',
     'maintainer': 'ITSA',
     'website': 'http://www.itsa.edu.mx',
     'depends': ['base'],
