e9f8395175d0f3cec0daddbf7eca5821868d19d8
JorgeCerdaMurillo <cerdamurillojorge00@gmail.com>
2022-03-15 10:35:02 -0600

    ModifDeJorgeHoy

--- profesores.py
+++ profesores.py
@@ -4,14 +4,13 @@ from odoo import models,fields
 class profesores(models.Model):
     _name = 'escolares.profesores'
 
-    name = fields.Char(string='Matricula')
-    nombre = fields.Char(string='Nombre y Apellido')
+    name = fields.Char(string='Clave')
+    nombre = fields.Char(string='Nombre y Apellidos')
     direccion = fields.Char(string='Dirección')
     telefono = fields.Char(string='Teléfono')
     email = fields.Char(string='Email')
     sexo = fields.Selection([('f','Femenino'),('m','Masculino')],string='Sexo')
     edad = fields.Integer(string='Edad')
-    materia = fields.Char(string='Materia a impartir')
 
 
     _sql_constraints = [
